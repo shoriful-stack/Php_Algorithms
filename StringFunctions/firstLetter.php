11bb3c2c3e7a5c154ecccf126b3eb952ea3b85ce
Sa18-f <sisazid182713@gmail.com>
2024-09-14 15:08:55 +0600

    first letter convertion added

---
+++ StringFunctions/firstLetter.php
@@ -0,0 +1,16 @@
+<!-- ucfirst() and lcfirst()
+Description: Capitalizes the first letter of a string (ucfirst()) or converts it to lowercase (lcfirst()). -->
+
+<!-- First letter convert into Uppercase -->
+<?php 
+$string = "sazid";
+$toUpper = ucfirst($string);
+echo $toUpper;
+?>
+
+<!-- First letter convert into lowercase  -->
+<?php 
+$string = "Sazid";
+$toLower = lcfirst($string);
+echo $toLower;
+?>
