d9d0310410bbe60a7c1944b0e8fe8b39682b7e0b
Sa18-f <sisazid182713@gmail.com>
2024-09-14 15:02:03 +0600

    explode and implode functions added

---
+++ StringFunctions/explode.php
@@ -0,0 +1,9 @@
+<!-- explode()
+Description: Splits a string by a specified delimiter into an array. -->
+
+<?php 
+$string = "alk, fks, lds";
+$array = explode(",", $string);
+print_r($array);
+
+?>
---
+++ StringFunctions/implode.php
@@ -0,0 +1,8 @@
+<!-- implode()
+Description: Joins array elements into a string with a specified delimiter. -->
+
+<?php 
+$array = ["apple", "banana", "orange"];
+$string = implode(",", $array);
+echo $string;
+?>
