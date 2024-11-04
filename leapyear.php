9baa1050e77ec9247957e63789f0524ff1af7431
shoriful-stack <sisazid182713@gmail.com>
2024-11-04 18:09:33 +0600

    new leapyear problem addded

--- leapyear.php
+++ leapyear.php
@@ -11,5 +11,14 @@ function leapYear($year){
     return ($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0);
 }
 
-echo leapYear(2018) ? "Leap Year" : "Not Leap Year";
+echo leapYear(2012) ? "Leap Year" : "Not Leap Year";
+echo "\n";
+
+
+$year = 2012;
+if($year % 4 === 0 && ($year % 100 !== 0 || $year % 400 === 0)){
+    echo "{$year} is a leap year";
+}else{
+    echo "{$year} is not a leap year";
+}
 ?>
