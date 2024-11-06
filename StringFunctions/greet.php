824d4e7846430a841eab1de257594260f8b1af15
shoriful-stack <sisazid182713@gmail.com>
2024-11-06 15:15:59 +0600

    new function added

---
+++ StringFunctions/greet.php
@@ -0,0 +1,7 @@
+<?php 
+// Declare a function named 'greet' that takes one parameter, 'name'. The function should print a greeting message with the name passed to it when it is called.
+
+function greet($name){
+    echo 'Good Morning '. $name . "!";
+}
+greet("Shoriful");
--- problem_solving/odd.php
+++ problem_solving/odd.php
@@ -11,6 +11,13 @@ function getOdd($numbers){
 
 $numbers = [1,2,3,4,5,6,7,8,9];
 $result = getOdd($numbers);
-echo "The odd numbers of the numbers are:".implode("," ,$oddNum);
+// echo "The odd numbers of the numbers are:".implode("," ,$oddNum);
 
 ?>
+<?php 
+// Create a PHP script using a loop to print all odd numbers between 1 and 10.
+for($i = 1; $i <= 10; $i++ ){
+    if($i % 2== 1){
+       echo $i . "\n";
+    }
+}
